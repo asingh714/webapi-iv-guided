1558827945bff5247d75e7237326d7f81cb0c8e5
Aman <amsingh714@gmail.com>
2019-03-21 13:17:59 -0400

    edited port

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = require('./api/server.js');
 // a hosting service might assign a port dynamically. 
 // Environment variable are system wide. 
 const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING 
 server.listen(port, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+  console.log(`\n*** ${greeting} http://localhost:${port} ***\n`);
 });
