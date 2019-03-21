34e6666200c04806c2e6cb56446a9055767d6e91
Aman <amsingh714@gmail.com>
2019-03-21 09:14:49 -0400

    added port

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
+// a hosting service might assign a port dynamically. 
+// Environment variable are system wide. 
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
